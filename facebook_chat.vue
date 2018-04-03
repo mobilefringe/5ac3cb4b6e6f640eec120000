ef5083922e55f32544071e93ab4fb85219949887
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-03 16:54:28 -0400

    Twin Pines Mall 2.0 1.0  /facebook_chat.vue

---
+++ facebook_chat.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="dataLoaded" id="overlay">
+        <div class="loading-container">
+            <vue-simple-spinner size="huge" :message="property.name + 'is loading...'"></vue-simple-spinner>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "vue-simple-spinner"], function (Vue, Vuex, Spinner) {
+        Vue.component("vue-simple-spinner", Spinner);
+        return Vue.component("loader", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    dataLoaded: false
+                };
+            },
+            created (){
+                // this.loadData().then(response => {
+                    this.dataLoaded = true;
+                // });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ])
+            }
+        });
+    });
+</script>
