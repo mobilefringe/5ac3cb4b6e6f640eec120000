756e19b7255708f72680e8d4976a194595444003
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-04 15:48:59 -0400

    Twin Pines Mall 2.0 1.0  /chatbot.vue

--- chatbot.vue
+++ chatbot.vue
@@ -1,14 +1,13 @@
 <template>
-    <!-- CHATBOT -->
     <div class="container">
         <!--Trigger the modal with a button -->
-        <div id="chaticon" class="animated swing infinite" @click="showChat = !showChat"> <!-- data-toggle="modal" data-target="#chat_modal" -->
+        <div id="chaticon" class="animated swing infinite" @click="showChat = !showChat">
             <input id="chatbot" type="image" src="//codecloud.cdn.speedyrails.net/sites/596e75bc6e6f6473f8be0000/image/png/1501602564000/chat_icon.png" />
         </div>
         <!--Modal -->
-        <div v-if="showChat" id="chat_modal" class="modal" :class="{ show_chat: showChat }"> <!-- tabindex="-1" role="dialog" aria-labelledby="chat_modalLabel" -->
+        <div v-if="showChat" id="chat_modal" class="modal" :class="{ show_chat: showChat }">
             <div class="lab-modal-body modal-sm">
-                <button type="button" class="close_chatbot"  @click="showChat = !showChat"> <!-- data-dismiss="modal" aria-label="Close" -->
+                <button type="button" class="close_chatbot"  @click="showChat = !showChat">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 <iframe id="chat_frame" :src="chatURL" width="100%" height="400px" frameBorder="0" allowfullscreen></iframe>
@@ -23,16 +22,9 @@
             template: template, // the variable template will be injected,
             data: function () {
                 return {
-                    // dataLoaded: false,
                     showChat: false,
                     chatURL: 'http://preview-twinpines.codecloudapp.com/chat.html',
                 };
-            },
-            created (){
-                console.log(this.showChat)
-                // this.loadData().then(response => {
-                    // this.dataLoaded = true;
-                // });
             },
             watch: {
                 showChat: function() {
