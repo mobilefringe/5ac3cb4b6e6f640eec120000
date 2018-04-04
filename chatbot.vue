6d1e9f59fbf0bd8093dcbf61bdc4217398fbeae6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-04 12:40:21 -0400

    Twin Pines Mall 2.0 1.0  /chatbot.vue

--- chatbot.vue
+++ chatbot.vue
@@ -2,16 +2,16 @@
     <!-- CHATBOT -->
     <div class="container">
         <!--Trigger the modal with a button -->
-        <div id="chaticon" class="animated swing infinite" data-toggle="modal" data-target="#chat_modal">
+        <div id="chaticon" class="animated swing infinite" data-toggle="modal" data-target="#chat_modal" @click="showChat = !showChat">
             <input id="chatbot" type="image" src="//codecloud.cdn.speedyrails.net/sites/596e75bc6e6f6473f8be0000/image/png/1501602564000/chat_icon.png" />
         </div>
         <!--Modal -->
-        <div class="modal" id="chat_modal" tabindex="-1" role="dialog" aria-labelledby="chat_modalLabel">
+        <div v-if="showChat" class="modal" id="chat_modal" tabindex="-1" role="dialog" aria-labelledby="chat_modalLabel">
             <div class="lab-modal-body modal-sm">
                 <button type="button" class="close_chatbot" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-                <iframe id="chat_frame" src='https://emmachat.codecloudapp.com/chat' height="400px"></iframe>
+                <iframe id="chat_frame" :src="chatURL" width="100%" height="400px" frameBorder="0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
@@ -23,7 +23,9 @@
             template: template, // the variable template will be injected,
             data: function () {
                 return {
-                    dataLoaded: false
+                    dataLoaded: false,
+                    showChat: false,
+                    chatURL: 'http://preview-twinpines.codecloudapp.com/chat',
                 };
             },
             created (){
