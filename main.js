8ef4ca04e1f3d891125c8c991bd45248119e0b44
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-04 12:42:17 -0400

    Twin Pines Mall 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ require.config({
     }
 });
 
-require(["Vue", "vuex", "vue2-filters", "vue_router", "routes", "datastore", "vue-i18n", "locales", "jquery", "moment", "moment-timezone", "vue-moment", "vue-meta", "vuex-router-sync", "vue-social-sharing", "vue-select", "vue!search-component", "vue-simple-spinner", "vue!loader.vue", "vue!header.vue", "vue!footer.vue"], function (Vue, Vuex, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages, $, moment, tz, VueMoment, Meta, VuexRouterSync, SocialSharing, VueSelect, Spinner, Loader, HeaderComponent, FooterComponent) {
+require(["Vue", "vuex", "vue2-filters", "vue_router", "routes", "datastore", "vue-i18n", "locales", "jquery", "moment", "moment-timezone", "vue-moment", "vue-meta", "vuex-router-sync", "vue-social-sharing", "vue-select", "vue!search-component", "vue-simple-spinner", "vue!loader.vue", "vue!header.vue", "vue!footer.vue", "vue!chatbot.vue"], function (Vue, Vuex, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages, $, moment, tz, VueMoment, Meta, VuexRouterSync, SocialSharing, VueSelect, Spinner, Loader, HeaderComponent, FooterComponent, ChatbotComponent]) {
     
     
         
