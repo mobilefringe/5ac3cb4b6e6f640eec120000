ae7ccdb00717928edcf359364e08fd8f484d0bef
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-04 11:30:45 -0400

    Twin Pines Mall 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -182,6 +182,7 @@ require(["Vue", "vuex", "vue2-filters", "vue_router", "routes", "datastore", "vu
                 stickyMenu: false,
                 isSticky: 350,
                 showChat: false,
+                chatURL: 'https://emmachat.codecloudapp.com/chat',
             }
         },
         created() {
