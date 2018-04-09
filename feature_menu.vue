7e7fe7f0778a2219b35e2bfc4013ac5fdd6c9ec9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-09 11:47:12 -0400

    Twin Pines Mall 2.0 1.0  /feature_menu.vue

---
+++ feature_menu.vue
@@ -0,0 +1,68 @@
+<template>
+    <div v-if="dataLoaded">
+        <div class="wrapper">
+            <ul>
+                <li>
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    <div class="slider">
+                        <p>home</p>
+                    </div>
+                </li>
+                <li>
+                    <i class="fa fa-file" aria-hidden="true"></i>
+                    <div class="slider">
+                        <p>files</p>
+                    </div>
+                </li>
+                <li class="home">
+                    <i class="fa fa-download" aria-hidden="true"></i>
+                    <div class="slider">
+                        <p>downloads</p>
+                    </div>
+                </li>
+                <li>
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <div class="slider">
+                        <p>notifications</p>
+                    </div>
+                </li>
+                <li>
+                    <i class="fa fa-cog" aria-hidden="true"></i>
+                    <div class="slider">
+                        <p>settings</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue"], function (Vue) {
+        return Vue.component("feature-menu", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    dataLoaded: false,
+                };
+            },
+            created () {
+                this.loadData().then(response => {
+                   
+                    
+                    this.dataLoaded = true;
+                });
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: "http://billingsbridge.mallmaverick.com/api/v3/billings/social.json"})]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
