c076d60b3724f3fe725e613ffc830dd19233f82b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-11 16:42:22 -0400

    Twin Pines Mall 2.0 1.0  /keele-banner.vue

--- keele-banner.vue
+++ keele-banner.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="keele banners">
-        <div class="prev"></div>
-        <slick ref="slick" :options="keeleOptions">
-			<div v-for="banner in banners" v-if="banners">
-			    <div class="slide">
-                    <div class="slide__content">
-                        <figure class="slide__figure">
-                            <div class="slide__img" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
-                        </figure>
-                        <header class="slide__header">
-                            <h2 class="slide__title">
-                                <span class="title-line"><span>Click, Key</span></span>
-                                <span class="title-line"><span>Or Scroll Fool</span></span>
-                            </h2>
-                        </header>
-                    </div>
-                </div>
-			</div>
-		</slick>
-		<div class="next"></div>
-    </div>
+    <section class="slides"> 
+        <section class="slides-nav">
+            <nav class="slides-nav__nav">
+                <button class="slides-nav__prev js-prev">Prev</button>
+                <button class="slides-nav__next js-next">Next</button>
+            </nav>
+        </section>
+
+        <section class="slide is-active">
+            <div class="slide__content">
+                <figure class="slide__figure">
+                    <div class="slide__img" style="background-image: url(https://source.unsplash.com/nfTA8pdaq9A/2000x1100)"></div>
+                </figure>
+                <header class="slide__header">
+                    <h2 class="slide__title">
+                        <span class="title-line">
+                            <span>Click, Key</span>
+                        </span>
+                        <span class="title-line">
+                            <span>Or Scroll Fool</span>
+                        </span>
+                    </h2>
+                </header>
+            </div>
+        </section>
+    </section>
 </template>
 
 <style>
