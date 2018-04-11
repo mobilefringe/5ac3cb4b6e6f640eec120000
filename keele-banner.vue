f7a0df5e71ef89133e8fa7ad0cc2ec63313a6128
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-11 15:05:07 -0400

    Twin Pines Mall 2.0 1.0  /keele-banner.vue

--- keele-banner.vue
+++ keele-banner.vue
@@ -16,18 +16,6 @@
                         </header>
                     </div>
                 </div>
-			
-			
-			
-			    <!--<div class="banner" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">-->
-                    <!--<div class="banner-legend"></div> -->
-                    <!--<div class="banner-content"> -->
-                    <!--    <div class="banner-content-txt"> -->
-                    <!--        <h3>{{ banner.name }}</h3> -->
-                    <!--        <p>{{ banner.description }}</p> -->
-                    <!--    </div> -->
-                    <!--</div> -->
-                <!--</div> -->
 			</div>
 		</slick>
 		<div class="next"></div>
@@ -39,7 +27,7 @@
     position: relative;
 }
 
-.slide{
+.slide {
     padding: 0;
     width: 100%;
     height: 100vh;
@@ -48,10 +36,62 @@
     transition: z-index 1s ease;
 }
 /* Bring active layer to foreground */
-.slick-active.is-active{
+.slick-active.is-active {
     z-index: 19;
     transition: z-index 1s ease;
 }
+.slide__content {
+    position: relative;
+    margin: 0 auto;
+    height: $slide-content-size-sm;
+    width: $slide-content-size-sm;
+    top: (100% - $slide-content-size-sm) / 2;
+}
+.slide__figure {
+    z-index: 7;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    height: 100%;
+    width: 100%;
+    //overflow-y: hidden;
+    transition: transform 0.5s $ease-cb-2;
+}
+/* Scale Image */
+    /*.is-sliding &{*/
+    /*  transform: scale(0.8);*/
+    /*  transition: transform 0.5s $ease-cb-2;*/
+    /*}*/
+
+.slide__img {
+    position: relative;
+    display: block;
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: 50%;
+    -webkit-backface-visibility: hidden;
+    height: 0%;
+    width: 100%;
+    filter: grayscale(0%);
+    transition: height 1s 1.4s $ease-cb-2, filter 0.4s 0.1s ease;
+}
+
+.slick-active.is-active .slide__img {
+    height: 100%;
+    opacity: 1;
+    transition: height .5s .3s $ease-cb-3, filter 0.4s 0.1s ease;
+}
+    /*.is-sliding &{*/
+    /*  filter: grayscale(100%);*/
+    /*}*/
+
+    /*@media (min-width: $mq-med){*/
+    /*  height: $slide-content-size;*/
+    /*  width: $slide-content-size;*/
+    /*  top: (100% - $slide-content-size) / 2;*/
+    /*}*/
+
 </style>
 
 <script>
