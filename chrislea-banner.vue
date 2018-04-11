0f4ac74285a67c78f2a61c6f2085275d3bac7975
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-11 14:03:44 -0400

    Twin Pines Mall 2.0 1.0  /chrislea-banner.vue

--- chrislea-banner.vue
+++ chrislea-banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="banners chrislea banner_option_2">
+    <div class="banners chrislea">
         <div class="prev"></div>
         <slick ref="slick" :options="chrisleaOptions" key="banners2">
 			<div class="" v-for="banner in banners" v-if="banners">
@@ -26,7 +26,62 @@
 </template>
 
 <style>
+.chrislea {
+    margin: 20px auto;
+    width: 100%;
+    position: relative;
+    box-sizing: border-box;
+}
+.banner {
+    opacity: 0;
+    position: relative;
+    z-index: 0;
+    transition: all 3s ease;
+    width: 100%;
+}
+.details {
+	padding: 20px;
+	color: #FFF;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	z-index: 3;
+}
+.details span {
+	display: block;
+	opacity: 0;
+	position: relative;
+	top: 100px;
+	transition-property: top, opacity;
+	transition-duration: 0.3s;
+	transition-delay: 0s;
+}
+.details .title h3 {
+    font-weight: 600;
+    line-height: 1;
+}
+.details .title .info {
+	margin-top: 5px;
+}
 
+.slick-slide.slick-active .banner {
+    opacity: 1;
+}
+.slick-slide.slick-active .banner .details:before, .slick-slide.slick-active .banner .details span {
+	opacity: 1;
+}
+.slick-slide.slick-active .banner .details:before {
+	top: 50%;
+}
+.slick-slide.slick-active .banner .details span {
+	top: 0;
+}
+.slick-slide.slick-active .banner .details .title {
+	transition-delay: 0.35s;
+}
+.slick-slide.slick-active .banner .details .title .info {
+	transition-delay: 0.45s;
+}
 </style>
 
 <script>
