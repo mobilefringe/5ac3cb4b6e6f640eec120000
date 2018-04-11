31540ba71fcd8bd622f4116ed5c785caea63a858
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-11 12:58:25 -0400

    Twin Pines Mall 2.0 1.0  /weston-banner.vue

--- weston-banner.vue
+++ weston-banner.vue
@@ -113,8 +113,8 @@
         height: 300px;
     }   
     .banner-legend {
-        border: 300px solid transparent;
-        border-left: 300px solid rgba(0, 0, 0, .7);
+        /*border: 300px solid transparent;*/
+        /*border-left: 300px solid rgba(0, 0, 0, .7);*/
     }
 }
 </style>
