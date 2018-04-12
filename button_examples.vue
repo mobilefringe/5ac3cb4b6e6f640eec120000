645872139e3082670a1fdc2d398c866ee071e28f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-12 09:21:26 -0400

    Twin Pines Mall 2.0 1.0  /button_examples.vue

--- button_examples.vue
+++ button_examples.vue
@@ -25,16 +25,47 @@
 </template>
 
 <style>
-    .btn {
-        position: relative;
-        display: block;
-        overflow: hidden;
-        height: 30px;
-        max-width: 250px;
-        margin: 1rem auto;
-        text-transform: uppercase;
-        border: 1px solid #000;
-    }
+    /*.btn {*/
+    /*    position: relative;*/
+    /*    display: block;*/
+    /*    overflow: hidden;*/
+    /*    height: 30px;*/
+    /*    max-width: 250px;*/
+    /*    margin: 1rem auto;*/
+    /*    text-transform: uppercase;*/
+    /*    border: 1px solid #000;*/
+    /*}*/
+    /*.btn-slice {*/
+    /*    $btn-color: random-color($max:200);*/
+    /*    $btn-color-dark: shade($btn-color, 40%);*/
+    /*    color: tint($btn-color);*/
+    /*    @include triangle(up-right, 0, $btn-color-dark, true);*/
+    /*    @include triangle(down-left, 0, $btn-color-dark, true, before);*/
+    /*}*/
+    /*.btn-slice:before, .btn-slice:after {*/
+    /*    content: '';*/
+    /*    position: absolute;*/
+    /*    width: 0;*/
+    /*    height: 0;*/
+    /*    border: 0 solid;*/
+    /*    transform: rotate(360deg);*/
+    /*}*/
+    /*.btn-slice:before {*/
+    /*    bottom: 0;*/
+    /*    left: 0;*/
+    /*    border-color: transparent transparent transparent $btn-color-dark;*/
+    /*}*/
+    /*.btn-slice:after {*/
+    /*    top: 0;*/
+    /*    right: 0;*/
+    /*    border-color: transparent $btn-color-dark transparent transparent;*/
+    /*}*/
+    /*.btn-slice:hover {*/
+    /*    color: tint($btn-color, 75%);*/
+    /*}*/
+    /*.btn-slice:hover .btn-slice:before, .btn-slice:hover .btn-slice:after {*/
+    /*    border-width: $btn-height $btn-width*1.05;*/
+    /*}*/
 </style>
 <script>
     define(["Vue"], function(Vue) {
