319a7d68ed78860253d419828a96ca3b7c14e454
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-16 17:09:45 -0400

    Twin Pines Mall 2.0 1.0  /keele-banner.vue

--- keele-banner.vue
+++ keele-banner.vue
@@ -1,52 +1,161 @@
+<!--<template>-->
+<!--    <section class="slides"> -->
+<!--        <section class="slides-nav">-->
+<!--            <nav class="slides-nav__nav">-->
+<!--                <button class="slides-nav__prev js-prev">Prev</button>-->
+<!--                <button class="slides-nav__next js-next">Next</button>-->
+<!--            </nav>-->
+<!--        </section>-->
+<!--        <section v-for="(banner, index) in banners" v-if="banners" class="slide" :class="{ 'is-active': index === 0 }">-->
+<!--            <div class="slide__content">-->
+<!--                <figure class="slide__figure">-->
+<!--                    <div class="slide__img" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>-->
+<!--                </figure>-->
+<!--                <header class="slide__header">-->
+<!--                    <h2 class="slide__title">-->
+<!--                        <span class="title-line">-->
+<!--                            <span>Click, Key</span>-->
+<!--                        </span>-->
+<!--                        <span class="title-line">-->
+<!--                            <span>Or Scroll Fool</span>-->
+<!--                        </span>-->
+<!--                    </h2>-->
+<!--                </header>-->
+<!--            </div>-->
+<!--        </section>-->
+<!--    </section>-->
+<!--</template>-->
+
+<!--<style>-->
+
+<!--</style>-->
+
+<!--<script>-->
+<!--    define(["Vue", "jquery", "slice"], function (Vue, $, SliceSlider) {-->
+<!--        console.log(SliceSlider)-->
+<!--        return Vue.component("keele-banner", {-->
+            <!--template: template, // the variable template will be injected,-->
+<!--            props:['banners'],-->
+<!--            data: function() {-->
+<!--                return {-->
+                    
+<!--                }-->
+<!--            },-->
+<!--            mounted() {-->
+<!--                SliceSlider();-->
+<!--            },-->
+<!--            created(){-->
+                <!--// SliceSlider.init();    -->
+<!--            }-->
+<!--        });-->
+<!--    });-->
+<!--</script>-->
 <template>
-    <section class="slides"> 
-        <section class="slides-nav">
-            <nav class="slides-nav__nav">
-                <button class="slides-nav__prev js-prev">Prev</button>
-                <button class="slides-nav__next js-next">Next</button>
-            </nav>
-        </section>
-        <section v-for="(banner, index) in banners" v-if="banners" class="slide" :class="{ 'is-active': index === 0 }">
-            <div class="slide__content">
-                <figure class="slide__figure">
-                    <div class="slide__img" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
-                </figure>
-                <header class="slide__header">
+	<div class="row page_content" id="slide_container">
+	    <main>
+            <section class="slides"> 
+              
+              <section class="slides-nav">
+                <nav class="slides-nav__nav">
+                  <button class="slides-nav__prev js-prev" ref="js_prev" @click="prevSlide">Prev</button>
+                  <button class="slides-nav__next js-next" ref="js_next" @click="nextSlide">Next</button>
+                </nav>
+              </section>
+            
+              <section class="slide" v-for = "(banner,index) in bannerlist" :class="{'is-active': (index==0)}">
+                <div class="slide__content">
+                  <figure class="slide__figure"><div class="slide__img" :style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div></figure>
+                  <header class="slide__header">
                     <h2 class="slide__title">
-                        <span class="title-line">
-                            <span>Click, Key</span>
-                        </span>
-                        <span class="title-line">
-                            <span>Or Scroll Fool</span>
-                        </span>
+                      <span class="title-line"><span>{{banner.name}}</span></span>
+                      <span class="title-line"><span>{{banner.description}}</span></span>
                     </h2>
-                </header>
-            </div>
-        </section>
-    </section>
+                  </header>
+                </div>
+              </section>
+            </section>
+        </main>
+    </div>
 </template>
 
-<style>
-
-</style>
-
 <script>
-    define(["Vue", "jquery", "slice"], function (Vue, $, SliceSlider) {
-        console.log(SliceSlider)
-        return Vue.component("keele-banner", {
-            template: template, // the variable template will be injected,
-            props:['banners'],
-            data: function() {
-                return {
-                    
+    define(["Vue", "jquery"], function(Vue, $) {
+        return Vue.component("slice-slider", {
+            template: template, // the variable template will be injected
+            props: {
+                bannerlist: {
+                    type: Array,
+                    required: true
+                },
+                delta: {
+                    type: Number,
+                    default: 0
+                },
+                autoPlaySpeed: {
+                    type:Number,
+                    default: 10000
                 }
             },
-            mounted() {
-                SliceSlider();
+            data () {
+                return {
+                    currentSlideIndex: 0,
+                    scrollThreshold: 40,
+                    slides: [],
+                    numSlides: this.bannerlist.length
+                }  
             },
-            created(){
-                // SliceSlider.init();    
+            mounted() {
+                this.slides = $('.slide');
+                this.interval = setInterval(function () {
+                    this.nextSlide();
+                }.bind(this), this.autoPlaySpeed); 
+            },
+            methods: {
+                prevSlide() {
+                    // If on first slide, loop to last
+                    if (this.currentSlideIndex <= 0) {
+                        this.currentSlideIndex = this.numSlides;
+                    }
+                    this.currentSlideIndex--;
+
+                    this.showSlide();
+                    console.log("sliding to prev", this.currentSlideIndex);
+                },
+                nextSlide() {
+                    this.currentSlideIndex++;
+
+                    // If on last slide, loop to first
+                    if (this.currentSlideIndex >= this.numSlides) {
+                        this.currentSlideIndex = 0;
+                    }
+
+                    this.showSlide();
+                    console.log("sliding to next", this.currentSlideIndex);
+                },
+                showSlide() {
+                    var vm = this;
+                    // reset
+                    this.delta = 0;
+                    // Bail if we're already sliding
+                    if ($('#slide_container').hasClass('is-sliding')) {
+                        return;
+                    }
+                    // Loop through our slides
+                    this.slides.each(function(i, slide) {
+                        // Toggle the is-active class to show slide
+                        $(slide).toggleClass('is-active', (i === vm.currentSlideIndex));
+                        $(slide).toggleClass('is-prev', (i === vm.currentSlideIndex - 1));
+                        $(slide).toggleClass('is-next', (i === vm.currentSlideIndex + 1));
+                        
+                        // Add and remove is-sliding class
+                        $('#slide_container').addClass('is-sliding');
+
+                        setTimeout(function() {
+                            $('#slide_container').removeClass('is-sliding');
+                        }, 1000);
+                    });
+                }
             }
-        });
+        })
     });
 </script>
