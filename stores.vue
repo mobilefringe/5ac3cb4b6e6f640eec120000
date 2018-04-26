33636c7d68f42b401103f1dc048c32dae44d152d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-26 12:48:57 -0400

    Twin Pines Mall 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -60,10 +60,10 @@
             							<span v-else>
             							    <img class="store_no_hover_img" :src="store.store_front_url_abs"/>    
             							</span>
-        								<div class="store_coming_soon" v-if="store.is_coming_soon_store">
+        								<div class="store_coming_soon" v-if="store.is_coming_soon_store && !store.is_new_store">
         									<div class="new_store">{{$t("stores_page.coming_soon")}}</div>
         								</div>
-        								<div class="store_coming_soon" v-if="store.is_new_store">
+        								<div class="store_coming_soon" v-if="store.is_new_store && !store.is_coming_soon_store">
         									<div class="new_store">{{$t("stores_page.new_store")}}</div>
         								</div>
         							</router-link>
