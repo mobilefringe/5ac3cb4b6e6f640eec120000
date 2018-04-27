cf968cb99cbeacf0a851594c23de9bc9c09ff755
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-27 12:45:20 -0400

    Twin Pines Mall 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -96,7 +96,7 @@
                 }
             },
             created (){
-                console.log(this.$route)
+                // console.log(this.$route)
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Directory Banner').images;
                     if(temp_repo != null) {
