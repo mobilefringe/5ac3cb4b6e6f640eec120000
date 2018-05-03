1c4981e9eaf6e873afaf56bdaaadda5a10ec876c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-03 11:18:36 -0400

    Twin Pines Mall 2.0 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -23,7 +23,7 @@
             				    <div v-else v-html="currentPromo.rich_description_2"></div>
             				</div>
             				<div class="promo_details_share">
-            				    <social-sharing v-if="currentPost" :url="shareURL(currentPost.slug)" :title="currentPost.title" :description="currentPost.body" :quote="truncate(currentPost.body)" twitter-user="NorthParkCenter" :media="currentPost.image_url" inline-template>
+            				    <social-sharing v-if="currentPost" :url="shareURL(currentPromo.slug)" :title="currentPromo.title" :description="currentPromo.body" :quote="truncate(currentPromo.body)" twitter-user="NorthParkCenter" :media="currentPromo.image_url" inline-template>
                                     <div class="blog-social-share">
                                         <network network="facebook">
                                             <i class="fa fa-facebook-square"></i>
