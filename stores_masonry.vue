9fa926f924b5836a38626338a68b19c2af7dc3c3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-25 17:39:58 -0400

    Twin Pines Mall 2.0 1.0  /stores_masonry.vue

--- stores_masonry.vue
+++ stores_masonry.vue
@@ -122,6 +122,14 @@
         display: none;
     }
     .store_logo_container:hover .store_details {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 90px;
+        width: 130px;
+        background: #000;
+        color: red;
         display: block;
     }
     .listView {
