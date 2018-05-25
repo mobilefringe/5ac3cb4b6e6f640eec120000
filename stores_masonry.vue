a0bbc2c059c53eaf9733c273f520ca481e9da716
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-25 15:29:59 -0400

    Twin Pines Mall 2.0 1.0  /stores_masonry.vue

--- stores_masonry.vue
+++ stores_masonry.vue
@@ -49,25 +49,29 @@
         			        <button @click="toggleView()">Toggle View</button>
         			    </div>
         			</div>
-        			<div v-masonry transition-duration="0.3s" item-selector=".stores-grid-item">
-                        <transition-group name="custom-classes-transition" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" tag="div">
-                            <div v-masonry-tile  v-for="store in filteredStores" :key="store" class="stores-grid-item">
-                        	    <div class="store_logo_container">
-                        	        <router-link :to="'/stores/'+ store.slug">
-                            			<img :src="store.store_front_url_abs" alt="">
-                            			<div class="store_tag store_promotion" v-if="store.total_published_promos">-->
-        									<div class="store_tag_text">{{$t("stores_page.promotion")}}</div>
-        								</div>
-        								<div class="store_tag coming_soon" v-if="!store.total_published_promos && !store.is_new_store && store.is_coming_soon_store">
-        									<div class="store_tag_text">{{$t("stores_page.coming_soon")}}</div>
-        								</div>
-        								<div class="store_tag new_store" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
-        									<div class="store_tag_text">{{$t("stores_page.new_store")}}</div>
-        								</div>
-                            		</router-link>
-                        	    </div>
-                            </div>
-                        </transition-group>
+        			<div v-if="logoView">
+            			<div v-masonry transition-duration="0.3s" item-selector=".stores-grid-item">
+                            <transition-group name="custom-classes-transition" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" tag="div">
+                                <div v-masonry-tile  v-for="store in filteredStores" :key="store" class="stores-grid-item">
+                            	    <div class="store_logo_container">
+                            	        <router-link :to="'/stores/'+ store.slug">
+                                			<img :src="store.store_front_url_abs" alt="">
+                                			<div class="store_tag store_promotion" v-if="store.total_published_promos">-->
+            									<div class="store_tag_text">{{$t("stores_page.promotion")}}</div>
+            								</div>
+            								<div class="store_tag coming_soon" v-if="!store.total_published_promos && !store.is_new_store && store.is_coming_soon_store">
+            									<div class="store_tag_text">{{$t("stores_page.coming_soon")}}</div>
+            								</div>
+            								<div class="store_tag new_store" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
+            									<div class="store_tag_text">{{$t("stores_page.new_store")}}</div>
+            								</div>
+                                		</router-link>
+                            	    </div>
+                                </div>
+                            </transition-group>
+                        </div>
+                    </div>
+                    <div v-if="listView">
                     </div>
         		</div>
 	        </div>
