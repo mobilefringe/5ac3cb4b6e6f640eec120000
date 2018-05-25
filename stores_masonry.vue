dc924509f482c19c85a9e1c83a296f04eb1a5d90
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-25 14:30:32 -0400

    Twin Pines Mall 2.0 1.0  /stores_masonry.vue

--- stores_masonry.vue
+++ stores_masonry.vue
@@ -47,7 +47,7 @@
         			<div v-masonry transition-duration="0.3s" item-selector=".stores-grid-item" class="hidden_phone">
                         <!--<div v-masonry-tile class="item" >-->
                             <transition-group name="custom-classes-transition" enter-active-class="animated fadeIn" tag="div">
-                                <div v-masonry-tile class="item" v-for="store in filteredStores" :key="store" class="stores-grid-item">
+                                <div v-masonry-tile  v-for="store in filteredStores" :key="store" class="stores-grid-item"> <!-- class="item" -->
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/stores/'+ store.slug">
                                 			<img :src="store.store_front_url_abs" alt="">
