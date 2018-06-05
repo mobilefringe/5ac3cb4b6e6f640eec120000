d230adea1aca733ffbfe44c310053c8eaf7ff934
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-05 17:55:38 -0400

    Twin Pines Mall 2.0 1.0  /ruby_package.vue

--- ruby_package.vue
+++ ruby_package.vue
@@ -1,1 +1,90 @@
-//silver
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loader v-if="!dataLoaded"></loader>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+        			<div class="site_container">
+        				<div class="header_content caps">
+        				    <p>{{ $t("stores_page.header_desc") }}</p>
+        					<h1>{{ $t("packages.sapphire") }}</h1>
+        				</div>
+        			</div>
+        		</div>
+        		<div class="site_container">
+        		    <h1 class="home_page_title caps">Banners</h1>
+        		    
+    		        <h3 class="home_page_title caps text-left">Chrislea Banner</h3>
+    		        <chrislea-banner :banners="banners" :key="1"></chrislea-banner>
+    		        
+        		    <div class="margin_60 hidden_phone"></div>
+    		        <h3 class="home_page_title caps text-left">Weston Banner</h3>
+    		        <weston-banner :banners="banners" :key="2"></weston-banner>
+    		        
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    // bronze
+    define(["Vue", "vuex", "vue!vue-slick", "vue!edgeley-banner", "vue!chrislea-banner", "vue!weston-banner", "vue!keele-banner", "vue!silmar-banner"], function(Vue, Vuex, slick, edgeleyBanner, chrisleaBanner, westonBanner, keeleBanner, silmarBanner) {
+        return Vue.component("sapphire-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    dataLoaded: false,
+                    banners: [
+                        { "image_url": "http://nikolaywerner.ru/files/2017-02/-nwf9500.jpg", "name": "Slide One", "description": "Description One" },
+                        { "image_url": "http://nikolaywerner.ru/files/2017-02/-nwf9506.jpg", "name": "Slide Two", "description": "Description Two" },
+                        { "image_url": "http://nikolaywerner.ru/files/2017-02/-nwf9449.jpg", "name": "Slide Three", "description": "Description Three" },
+                        { "image_url": "http://nikolaywerner.ru/files/2017-02/-nwf9541.jpg", "name": "Slide Four", "description": "Description Four" }
+                    ],
+                    slickOptions3: {
+                        arrows: false,
+                        autoplay: true,
+                        centerMode: true,
+                        centerPadding: '5%',
+                        cssEase:'ease-in-out',
+                        slidesToShow: 5,
+                    },
+                    pageBanner:null
+                }
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.dataLoaded = true;
+                    var temp_repo = this.findRepoByName('Directory Banner').images;
+                    if(temp_repo != null) {
+                        this.pageBanner = temp_repo[0];
+                    } else {
+                        this.pageBanner = "http://via.placeholder.com/1920x400/4f6726/4f6726";
+                    }
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'processedStores',
+                    'findRepoByName'
+                ]),
+                allStores() {
+                    var store_list = this.processedStores;
+                    this.filteredStores = store_list;
+                    return store_list
+                }
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
