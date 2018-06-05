91c04d1767ccb35c080db73f6db9f8c44021f3e2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-05 17:58:16 -0400

    Twin Pines Mall 2.0 1.0  /diamond_package.vue

--- diamond_package.vue
+++ diamond_package.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-    // silver
+    // gold
     define(["Vue", "vuex", "vue!vue-slick", "vue!edgeley-banner", "vue!chrislea-banner", "vue!weston-banner", "vue!keele-banner", "vue!silmar-banner"], function(Vue, Vuex, slick, edgeleyBanner, chrisleaBanner, westonBanner, keeleBanner, silmarBanner) {
         return Vue.component("ruby-component", {
             template: template, // the variable template will be injected
