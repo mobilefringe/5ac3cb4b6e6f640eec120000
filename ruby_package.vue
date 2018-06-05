ec7b86d6042d8d647175acaf3e6e43bba3ba3fb8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-05 17:56:46 -0400

    Twin Pines Mall 2.0 1.0  /ruby_package.vue

--- ruby_package.vue
+++ ruby_package.vue
@@ -14,12 +14,12 @@
         		<div class="site_container">
         		    <h1 class="home_page_title caps">Banners</h1>
         		    
-    		        <h3 class="home_page_title caps text-left">Chrislea Banner</h3>
-    		        <chrislea-banner :banners="banners" :key="1"></chrislea-banner>
+    		        <h3 class="home_page_title caps">Keele Banner</h3>
+    		        <keele-banner :banners="banners"></keele-banner>
     		        
-        		    <div class="margin_60 hidden_phone"></div>
-    		        <h3 class="home_page_title caps text-left">Weston Banner</h3>
-    		        <weston-banner :banners="banners" :key="2"></weston-banner>
+    		        <div class="margin_60 hidden_phone"></div>
+    		        <h3 class="home_page_title caps">Silmar Banner</h3>
+    		        <silmar-banner :bannerList="banners"></silmar-banner>
     		        
                 </div>
             </div>
