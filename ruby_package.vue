d103d7e59a86b67ed39336f9200f8d9bbd1156e5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-06 10:49:24 -0400

    Twin Pines Mall 2.0 1.0  /ruby_package.vue

--- ruby_package.vue
+++ ruby_package.vue
@@ -25,7 +25,7 @@
 
 <script>
     // silver
-    define(["Vue", "vuex", "vue!vue-slick", "vue!edgeley-banner", "vue!chrislea-banner", "vue!weston-banner", "vue!keele-banner", "vue!silmar-banner"], function(Vue, Vuex, slick, edgeleyBanner, chrisleaBanner, westonBanner, keeleBanner, silmarBanner) {
+    define(["Vue", "vuex", "vue!vue-slick", "vue!edgeley-banner", "vue!silton-banner"], function(Vue, Vuex, slick, edgeleyBanner, chrisleaBanner, westonBanner, keeleBanner, silmarBanner) {
         return Vue.component("ruby-component", {
             template: template, // the variable template will be injected
             data: function() {
