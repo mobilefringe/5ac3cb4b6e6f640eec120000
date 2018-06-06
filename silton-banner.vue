e9e556e16a2530872e430f91f178098d12ad9631
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-06 10:41:57 -0400

    Twin Pines Mall 2.0 1.0  /silton-banner.vue

--- silton-banner.vue
+++ silton-banner.vue
@@ -14,45 +14,6 @@
 				</div>
 			</div>
 			<!-- // Item -->
-			<!-- Item -->
-			<div class="item">
-				<div class="img-fill">
-					<img src="https://i.imgur.com/ESMjChq.jpg" alt="">
-					<div class="info">
-						<div>
-							<h3>Fully responsive Scales with its container</h3>
-							<h5>Donec id ornare dui. Aenean tristique condimentum elit, quis blandit nisl varius sit amet. Sed eleifend felis quis massa viverra</h5>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- // Item -->
-			<!-- Item -->
-			<div class="item">
-				<div class="img-fill">
-					<img src="https://i.imgur.com/TDxSvHH.jpg" alt="">
-					<div class="info">
-						<div>
-							<h3>Awesome Transtions With CSS3</h3>
-							<h5>Donec id ornare dui. Aenean tristique condimentum elit, quis blandit nisl varius sit amet. Sed eleifend felis quis massa viverra</h5>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- // Item -->
-			<!-- Item -->
-			<div class="item">
-				<div class="img-fill">
-					<img src="https://i.imgur.com/p1XZ3Mu.jpg" alt="">
-					<div class="info">
-						<div>
-							<h3>Separate settings per breakpoint</h3>
-							<h5>Donec id ornare dui. Aenean tristique condimentum elit, quis blandit nisl varius sit amet. Sed eleifend felis quis massa viverra</h5>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- // Item -->
 		</div>
 	</div>
 </template>
