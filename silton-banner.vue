1fca11410079c67d1b41f539b4138056526a5c88
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-06 10:50:28 -0400

    Twin Pines Mall 2.0 1.0  /silton-banner.vue

--- silton-banner.vue
+++ silton-banner.vue
@@ -208,19 +208,19 @@
             data: function() {
                 return {
                     siltonOptions: {
-                    autoplay:true,
-                    autoplaySpeed:10000,
-                    speed:600,
-                    slidesToShow:1,
-                    slidesToScroll:1,
-                    pauseOnHover:false,
-                    dots:true,
-                    pauseOnDotsHover:true,
-                    cssEase:'linear',
-                   // fade:true,
-                    draggable:false,
-                    prevArrow:'<button class="PrevArrow"></button>',
-                    nextArrow:'<button class="NextArrow"></button>', 
+                        autoplay:true,
+                        autoplaySpeed:10000,
+                        speed:600,
+                        slidesToShow:1,
+                        slidesToScroll:1,
+                        pauseOnHover:false,
+                        dots:true,
+                        pauseOnDotsHover:true,
+                        cssEase:'linear',
+                       // fade:true,
+                        draggable:false,
+                        prevArrow:'<button class="PrevArrow"></button>',
+                        nextArrow:'<button class="NextArrow"></button>'
                     }
                 }
             }
