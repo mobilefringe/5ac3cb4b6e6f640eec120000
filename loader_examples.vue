0a21c98aa482204ca0855c867c7454f7b20475ea
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-15 16:42:23 -0400

    Twin Pines Mall 2.0 1.0  /loader_examples.vue

--- loader_examples.vue
+++ loader_examples.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
         		<div class="site_container">
         		<div class="margin_40"></div>
-        		    <h3 class="home_page_title caps">Loaders</h3>
+        		    <h3 class="home_page_title caps">Ball Beat Loader</h3>
         		    <div class="text-center loaders_container">
         		        <div class="loader-inner ball-beat">
             		        <div></div>
