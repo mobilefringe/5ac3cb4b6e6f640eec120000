69df6c5d0512eda9a7f21764ee615c717dccf723
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-15 16:50:23 -0400

    Twin Pines Mall 2.0 1.0  /loader_examples.vue

--- loader_examples.vue
+++ loader_examples.vue
@@ -44,14 +44,14 @@
         		    </div>
         		    <div class="margin_40"></div>
         		    <h3 class="home_page_title caps">Square Spin Loader</h3>
-        		    <div class="text-center clearfix loaders_container">
-        		        <div class="loader-inner square-spin">
+        		    <div class="text-center loaders_container">
+        		        <div class="loader-inner clearfix square-spin">
             		        <div></div>
             		    </div>
         		    </div>
         		    <div class="margin_40"></div>
         		    <h3 class="home_page_title caps">Ball Clip Rotate Loader</h3>
-        		    <div class="text-center clearfix loaders_container">
+        		    <div class="text-center loaders_container">
         		        <div class="loader-inner ball-clip-rotate-multiple">
             		        <div></div>
             		        <div></div>
