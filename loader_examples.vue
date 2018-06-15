c542ed696068ceedddae05690c0e7a3ee6577d61
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-15 16:52:48 -0400

    Twin Pines Mall 2.0 1.0  /loader_examples.vue

--- loader_examples.vue
+++ loader_examples.vue
@@ -32,7 +32,7 @@
             		    </div>
         		    </div>
         		    <div class="margin_40"></div>
-        		    <h3 class="home_page_title caps">Ball Pulse Loader</h3>
+        		    <h3 class="home_page_title caps">Line Scale Pulse Loader</h3>
         		    <div class="text-center loaders_container">
         		        <div class="loader-inner clearfix line-scale-pulse-out-rapid">
             		        <div></div>
@@ -76,9 +76,9 @@
         margin:auto;
     }
     .ball-clip-rotate-multiple.clearfix.loader-inner {
-    position: absolute;
-    left: 50%;
-}
+        position: absolute;
+        left: 50%;
+    }
 </style>
 <script>
     define(["Vue"], function(Vue) {
