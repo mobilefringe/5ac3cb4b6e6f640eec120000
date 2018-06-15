213409da65107f81c29158d6b25f4b6cb0bfe4e8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-15 16:49:20 -0400

    Twin Pines Mall 2.0 1.0  /loader_examples.vue

--- loader_examples.vue
+++ loader_examples.vue
@@ -49,6 +49,17 @@
             		        <div></div>
             		    </div>
         		    </div>
+        		    <div class="margin_40"></div>
+        		    <h3 class="home_page_title caps">Ball Pulse Loader</h3>
+        		    <div class="text-center loaders_container">
+        		        <div class="loader-inner line-scale-pulse-out-rapid">
+            		        <div></div>
+            		        <div></div>
+            		        <div></div>
+            		        <div></div>
+            		        <div></div>
+            		    </div>
+        		    </div>
                 </div>
             </div>
         </transition>
@@ -65,7 +76,7 @@
         text-align: center;
     }
     .square-spin > div {
-        margin:autp;
+        margin:auto;
     }
 </style>
 <script>
