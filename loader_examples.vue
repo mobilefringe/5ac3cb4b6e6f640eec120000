dc69d2d9e9855bfc45966db846c51cf7a9f1f25a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-15 16:43:16 -0400

    Twin Pines Mall 2.0 1.0  /loader_examples.vue

--- loader_examples.vue
+++ loader_examples.vue
@@ -17,9 +17,9 @@
         		    <h3 class="home_page_title caps">Ball Grid BeatLoader</h3>
         		    <div class="text-center loaders_container">
         		        <div class="loader-inner ball-grid-beat">
-            		        <div></div>
-            		        <div></div>
-            		        <div></div>
+            		        <div></div><div></div><div></div>
+            		        <div></div><div></div><div></div>
+            		        <div></div><div></div><div></div>
             		    </div>
         		    </div>
                 </div>
