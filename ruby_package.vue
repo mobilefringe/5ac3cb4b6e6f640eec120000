4a603a71ac1dfe7f26e6000db50f3e0ab79681ce
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-18 14:57:18 -0400

    Twin Pines Mall 2.0 1.0  /ruby_package.vue

--- ruby_package.vue
+++ ruby_package.vue
@@ -30,26 +30,6 @@
             		    </div>
         		    </div>
         		    <div class="margin_40"></div>
-        		    <h3 class="home_page_title caps">Ball Pulse Loader</h3>
-        		    <div class="text-center loaders_container">
-        		        <div class="loader-inner ball-pulse">
-            		        <div></div>
-            		        <div></div>
-            		        <div></div>
-            		    </div>
-        		    </div>
-        		    <div class="margin_40"></div>
-        		    <h3 class="home_page_title caps">Line Scale Pulse Loader</h3>
-        		    <div class="text-center loaders_container">
-        		        <div class="loader-inner clearfix line-scale-pulse-out-rapid">
-            		        <div></div>
-            		        <div></div>
-            		        <div></div>
-            		        <div></div>
-            		        <div></div>
-            		    </div>
-        		    </div>
-        		    <div class="margin_40"></div>
         		    <h3 class="home_page_title caps">Ball Clip Rotate Loader</h3>
         		    <div class="text-center loaders_container">
         		        <div class="loader-inner clearfix ball-clip-rotate-multiple">
