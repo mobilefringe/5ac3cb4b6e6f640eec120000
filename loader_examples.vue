0045e78d1dc95b4b14b51780856b52f993ee2e3f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-18 11:51:36 -0400

    Twin Pines Mall 2.0 1.0  /loader_examples.vue

--- loader_examples.vue
+++ loader_examples.vue
@@ -96,7 +96,7 @@
         height: 60px;
     }
     .square-spin.logo_example_2 > div {
-        background-image: url("//codecloud.cdn.speedyrails.net/sites/5ac3cb4b6e6f640eec120000/image/png/1529333770000/twinpineslogo-Square.png");
+        background-image: url("//codecloud.cdn.speedyrails.net/sites/5ac3cb4b6e6f640eec120000/image/png/1522945678000/tplogo.png");
         background-color: transparent;
         width: 60px;
         height: 60px;
