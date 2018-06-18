6186eca35447f738653bc0867b74ef0f2bcd76f1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-18 12:18:58 -0400

    Twin Pines Mall 2.0 1.0  /loader_examples.vue

--- loader_examples.vue
+++ loader_examples.vue
@@ -63,14 +63,7 @@
             		        <div></div>
             		    </div>
         		    </div>
-        		    <div class="margin_40"></div>
-        		    <h3 class="home_page_title caps">Ball Clip Rotate Loader</h3>
-        		    <div class="text-center loaders_container">
-        		        <div class="loader-inner clearfix ball-clip-rotate-multiple">
-            		        <div></div>
-            		        <div></div>
-            		    </div>
-        		    </div>
+        		    
                 </div>
             </div>
         </transition>
