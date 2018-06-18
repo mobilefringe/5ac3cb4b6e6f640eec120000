d83c14a0ebca700abab5344969a35a9b6605fe7d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-18 14:53:29 -0400

    Twin Pines Mall 2.0 1.0  /sapphire_package.vue

--- sapphire_package.vue
+++ sapphire_package.vue
@@ -69,7 +69,7 @@
             		    </div>
         		    </div>
         		    <div class="margin_40"></div>
-        		    <h3 class="home_page_title caps">Ball Pulse Loader</h3>
+        		    <h3 class="home_page_title caps text-left sub_title">Ball Pulse Loader</h3>
         		    <div class="text-center loaders_container">
         		        <div class="loader-inner ball-pulse">
             		        <div></div>
