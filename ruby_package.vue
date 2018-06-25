c231aefb863dff363ee010bab9b3c8c2d63a71e9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-25 12:25:01 -0400

    Twin Pines Mall 2.0 1.0  /ruby_package.vue

--- ruby_package.vue
+++ ruby_package.vue
@@ -98,19 +98,19 @@
     /*.button_examples .hvr-hollow:hover,.button_examples .hvr-hollow:focus,.button_examples .hvr-hollow:active {*/
     /*    background: none;*/
     /*}*/
-    /*.button_examples button {*/
-    /*    padding: 12px 50px;*/
-    /*    min-width: 200px;*/
-    /*    height: 60px;*/
-    /*    position: relative;*/
-        /* overflow: hidden; */
-    /*    outline: none;*/
-        /* border: 3px solid #4f6726; */
-    /*    background: #e1e1e1;*/
-    /*    color: #4f6726;*/
-    /*    text-transform: uppercase;*/
-    /*    border: none;*/
-    /*}*/
+    .button_examples button {
+        padding: 12px 50px;
+        min-width: 200px;
+        height: 60px;
+        position: relative;
+         overflow: hidden; 
+        outline: none;
+         border: 3px solid #4f6726; 
+        background: #e1e1e1;
+        color: #4f6726;
+        text-transform: uppercase;
+        border: none;
+    }
 </style>
 <script>
     // silver
