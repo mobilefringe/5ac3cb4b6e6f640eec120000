67a3c093169cda3697e19a1e4bc9868dca949aec
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-25 17:15:48 -0400

    Twin Pines Mall 2.0 1.0  /ruby_package.vue

--- ruby_package.vue
+++ ruby_package.vue
@@ -70,7 +70,16 @@
         		    <h1 class="home_page_title caps">Feature Items</h1>
         		    <div class="row margin_30 button_examples">
         		        <div class="col-md-3 center">
-        		        
+                            <div class="post-module">
+                                <div class="thumbnail">
+                                    <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg' />
+                                    <div class="post-content">
+                                        <h1 class="title">City Lights in New York</h1>
+                                        <h2 class="sub_title">The city that never sleeps.</h2>
+                                        <p class="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                                    </div>  
+                                </div> 
+                            </div>
         		        </div>
         		    </div>
                 </div>
