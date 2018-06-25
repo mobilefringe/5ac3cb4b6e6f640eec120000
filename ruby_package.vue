6844c1cccbfa9df7c3d4baef8a1aca65d63e8f8d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-25 12:27:32 -0400

    Twin Pines Mall 2.0 1.0  /ruby_package.vue

--- ruby_package.vue
+++ ruby_package.vue
@@ -75,6 +75,7 @@
 <style>
     @import "https://mmvue.codecloudapp.com/loaders.css";
     @import "https://twinpines.codecloudapp.com/animate.css";
+    @import "/hover.css";
     .loaders_container {
         background-color: #4f6726;
         padding: 50px;
