02ca417ad7f16e8109078643edfb9b14ba9547ec
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-13 18:11:32 -0400

    Twin Pines Mall 2.0 1.0  /sapphire_package.vue

--- sapphire_package.vue
+++ sapphire_package.vue
@@ -71,7 +71,7 @@
         		    <h1 class="home_page_title caps">Feature Items</h1>
         		    <h3 class="home_page_title caps text-left sub_title">Mouse tracking hover</h3>
         		    <div class="text-center ">
-        		        <tracking-image-hover :featureItems=""></tracking-image-hover>
+        		        <tracking-image-hover :featureItems="feature_items"></tracking-image-hover>
         		    </div>
         		    
                 </div>
