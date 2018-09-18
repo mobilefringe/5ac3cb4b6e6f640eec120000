1be7f384803156271d06bcc02b96b46bf3fb04de
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-18 11:44:40 -0400

    Twin Pines Mall 2.0 1.0  /magnific-gallery.vue

--- magnific-gallery.vue
+++ magnific-gallery.vue
@@ -1,70 +1,63 @@
 <template>
 	<!-- Container for the images.-->
-    <div id="feature_container_two" class="container">
-        <article class='gallery'>
-  <a class='gallery-link' href='https://unsplash.it/1600/1000?image=1081'>
-    <figure class='gallery-image'>
-      <img height='1000' src='https://unsplash.it/1600/1000?image=1081' width='1600'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1000/1600?image=1014'>
-    <figure class='gallery-image'>
-      <img height='1600' src='https://unsplash.it/1000/1600?image=1014' width='1000'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1600/1000?image=267'>
-    <figure class='gallery-image'>
-      <img height='1000' src='https://unsplash.it/1600/1000?image=267' width='1600'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1400/1400?image=266'>
-    <figure class='gallery-image'>
-      <img height='1400' src='https://unsplash.it/1400/1400?image=266' width='1400'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1000/1000?image=634'>
-    <figure class='gallery-image'>
-      <img height='1000' src='https://unsplash.it/1000/1000?image=634' width='1000'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1600/1000?image=923'>
-    <figure class='gallery-image'>
-      <img height='1000' src='https://unsplash.it/1600/1000?image=923' width='1600'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1200/1600?image=682'>
-    <figure class='gallery-image'>
-      <img height='1600' src='https://unsplash.it/1200/1600?image=682' width='1200'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1400/1000?image=173'>
-    <figure class='gallery-image'>
-      <img height='1000' src='https://unsplash.it/1400/1000?image=173' width='1400'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-  <a class='gallery-link' href='https://unsplash.it/1000/1000?image=943'>
-    <figure class='gallery-image'>
-      <img height='1000' src='https://unsplash.it/1000/1000?image=943' width='1000'>
-      <figcaption>Photo caption</figcaption>
-    </figure>
-  </a>
-</article>
-<footer id='footer' role='contentinfo'>
-  <div class='container'>
-    <a class='logo' href='https://codepen.io/collection/XRoxGR' rel='home'>Calibration theme</a>
-    <a class='copy' href='https://mobilemarkup.com' target='_blank'>&copy; mobileMarkup.com</a>
-  </div>
-</footer>
-
-    </div>
+	<div id="feature_container_two" class="container">
+		<article class='gallery'>
+			<a class='gallery-link' href='https://unsplash.it/1600/1000?image=1081'>
+				<figure class='gallery-image'>
+					<img height='1000' src='https://unsplash.it/1600/1000?image=1081' width='1600'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1000/1600?image=1014'>
+				<figure class='gallery-image'>
+					<img height='1600' src='https://unsplash.it/1000/1600?image=1014' width='1000'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1600/1000?image=267'>
+				<figure class='gallery-image'>
+					<img height='1000' src='https://unsplash.it/1600/1000?image=267' width='1600'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1400/1400?image=266'>
+				<figure class='gallery-image'>
+					<img height='1400' src='https://unsplash.it/1400/1400?image=266' width='1400'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1000/1000?image=634'>
+				<figure class='gallery-image'>
+					<img height='1000' src='https://unsplash.it/1000/1000?image=634' width='1000'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1600/1000?image=923'>
+				<figure class='gallery-image'>
+					<img height='1000' src='https://unsplash.it/1600/1000?image=923' width='1600'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1200/1600?image=682'>
+				<figure class='gallery-image'>
+					<img height='1600' src='https://unsplash.it/1200/1600?image=682' width='1200'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1400/1000?image=173'>
+				<figure class='gallery-image'>
+					<img height='1000' src='https://unsplash.it/1400/1000?image=173' width='1400'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+			<a class='gallery-link' href='https://unsplash.it/1000/1000?image=943'>
+				<figure class='gallery-image'>
+					<img height='1000' src='https://unsplash.it/1000/1000?image=943' width='1000'>
+					<figcaption>Photo caption</figcaption>
+				</figure>
+			</a>
+		</article>
+	</div>
 </template>
 
 <style>
