db65cc74af77f4e47fbb3275c1d37c0e9530bce4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-11-30 09:37:13 -0500

    Twin Pines Mall 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -124,14 +124,14 @@
                 this.loadData().then(response => {
                     // this.popup = this.$store.state.popups[0];
 
-                    // var socialFeed = response[3].data;
-                    // var social_feed = socialFeed.social.instagram;
-                    // this.instaFeed = _.slice(social_feed, [0], [8]);
-                    // this.instaFeed.map(insta => {
-                    //     if(insta.caption != null){
-                    //         insta.caption.text = _.truncate(insta.caption.text, { 'length': 60, 'separator': ' ' });
-                    //     }
-                    // });
+                    var socialFeed = response[3].data;
+                    var social_feed = socialFeed.social.instagram;
+                    this.instaFeed = _.slice(social_feed, [0], [8]);
+                    this.instaFeed.map(insta => {
+                        if(insta.caption != null){
+                            insta.caption.text = _.truncate(insta.caption.text, { 'length': 60, 'separator': ' ' });
+                        }
+                    });
 
                     this.dataLoaded = true;
                 });
