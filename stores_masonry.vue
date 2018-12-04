9cb0f07ab05d8b5ecb158876a7194a1290754b16
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-12-04 16:15:09 -0500

    Twin Pines Mall 2.0 1.0  /stores_masonry.vue

--- stores_masonry.vue
+++ stores_masonry.vue
@@ -65,7 +65,9 @@
             									<div class="store_tag_text">{{$t("stores_page.new_store")}}</div>
             								</div>
             								<div class="store_details">
-            								    <div class="store_text">{{ store.name }}</div>    
+            								    <div class="store_text">
+            								        <h3>{{ store.name }}</h3>
+        								        </div>    
             								</div>
                                 		</router-link>
                             	    </div>
