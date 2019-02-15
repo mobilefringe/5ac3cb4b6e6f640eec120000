ab628616432bd2d5098b77302d1cda16d8fa8d42
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-15 12:40:34 -0500

    Twin Pines Mall 2.0 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -22,7 +22,7 @@
 					</div>
 					<div class="col-md-3 hidden_phone">
 					    <div class="header_search_container">
-    					    <search-component v-model="search" :list="searchList" :keys="keys" :suggestion-attribute="suggestionAttribute" :minMatchCharLength="3" :tokenize="true" @select="onOptionSelect" :autocomplete="false" @closeSearchContainer="closeSearchMenu">
+    					    <search-component v-model="search" :list="searchList" :keys="keys" :suggestion-attribute="suggestionAttribute" :minMatchCharLength="3" :tokenize="true" @select="onOptionSelect" :autocomplete="false" >
                                 <template slot="item" scope="option">
                                     <article class="media">
                                         <p>{{ option.data.name }}</p>
