d4ecd27e1d6f856ee8c92df8e43b2f055628c3f4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-12 13:51:06 -0400

    Twin Pines Mall 2.0 1.0  /tourism.vue

--- tourism.vue
+++ tourism.vue
@@ -23,16 +23,16 @@
         				<v-select :options="dropDownSelect" :searchable="false" :on-change="selectPage" class="category-select" placeholder="Select a Page" inputId="pageSelect"></v-select>
         			</div>
         			<div class="row">
-        			    <div :class="{'col-sm-12': !leasingSubpage && leasingContent.isActive, 'col-sm-6': leasingSubpage && leasingContent.isActive}">
+        			    <div :class="{'col-sm-12': !pageSubpage1 && leasingContent.isActive, 'col-sm-6': leasingSubpage && leasingContent.isActive}">
         			        <div v-if="pageContent">
         			            <div v-if="locale=='en-ca'" v-html="pageContent.body"></div>
         			            <div v-else v-html="pageContent.body_2"></div>
         			        </div>
         			    </div>
-        			   <div class="col-sm-6" v-if="leasingSubpage && leasingContent.isActive">
-        			       <div v-if="leasingSubpage">
-        			            <div v-if="locale=='en-ca'" v-html="leasingSubpage.body"></div>
-        			            <div v-else v-html="leasingSubpage.body_2"></div>
+        			   <div class="col-sm-6" v-if="pageSubpage1 && leasingContent.isActive">
+        			       <div v-if="pageSubpage1">
+        			            <div v-if="locale=='en-ca'" v-html="pageSubpage1.body"></div>
+        			            <div v-else v-html="pageSubpage1.body_2"></div>
         			        </div>
         			   </div>
         		    </div>
