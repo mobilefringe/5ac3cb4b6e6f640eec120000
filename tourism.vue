e86d961dae4132cd3b628db043f81ccc38b44287
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-12 14:52:13 -0400

    Twin Pines Mall 2.0 1.0  /tourism.vue

--- tourism.vue
+++ tourism.vue
@@ -35,7 +35,7 @@
         			   <!--         <div v-else v-html="pageSubpage1.body_2"></div>-->
         			   <!--     </div>-->
         			   <!--</div>-->
-        		    </div>
+        		    </div>{{currentPageContent.title}}
         		    <div class="row mall_info_images" v-if="currentPageContent.title == 'Travel Partners'">
         			    <div class="col-sm-3" v-for="item in travel_partners" v-if="travel_partners">
         			        <img class="max_width" :src="item.image_url" alt="" />
