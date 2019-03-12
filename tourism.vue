2174c8056d4b8294b6cf636d6ba64342236d9a2e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-12 13:34:20 -0400

    Twin Pines Mall 2.0 1.0  /tourism.vue

--- tourism.vue
+++ tourism.vue
@@ -52,7 +52,7 @@
 
 <script>
     define(["Vue", "vuex", "vue-select"], function(Vue, Vuex, VueSelect) {
-        return Vue.component("mall-info-component", {
+        return Vue.component("toruism-component", {
             template: template, // the variable template will be injected
             props:['locale'],
             data: function() {
